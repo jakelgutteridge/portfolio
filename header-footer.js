1779640d6f6929bd7c23a33a1c21fd1470856bbe
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2024-12-01 05:42:05 +0000

    Update header-footer.js

--- header-footer.js
+++ header-footer.js
@@ -74,3 +74,23 @@ const footerHTML = `
 // Inject the HTML content into the header and footer divs
 headerDiv.innerHTML = headerHTML;
 footerDiv.innerHTML = footerHTML;
+
+
+
+
+
+
+
+
+
+
+
+/* Test - injecting the nav menu */
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
